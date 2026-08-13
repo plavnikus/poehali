b40aa1be886c3dec6a68c4ba0c711cecb91db41c
plavnikus <stroy38.ru@gmail.com>
2026-08-13 21:22:14 +0800

    Add local family storage helpers

--- family.js
+++ family.js
@@ -1,2 +1,4 @@
 window.FAMILY=window.FAMILY||{};
 window.FAMILY.people=[['p1','Я'],['p2','Семья 2'],['p3','Семья 3']];
+window.FAMILY.get=function(){try{return JSON.parse(localStorage.getItem('poehali_family')||'{}')}catch(e){return{}}};
+window.FAMILY.save=function(x){localStorage.setItem('poehali_family',JSON.stringify(x))};
