f8a6d3ac457848dd0b7774e20b7cc50f610e3c03
plavnikus <stroy38.ru@gmail.com>
2026-08-17 01:12:54 +0800

    Use direct Google Maps place link for Buddha

--- place-nav.js
+++ place-nav.js
@@ -4,7 +4,7 @@ window.PLACE_NAV={
   'tazh-3':{lat:52.925877,lon:106.759206,label:'Тажеранская обзорная площадка',source:'official route passport'},
   'tazh-4':{lat:52.951578,lon:106.749014,label:'Тажеранские скалы-останцы',source:'official route passport'},
   'tazh-5':{lat:52.719069,lon:106.533435,label:'Залив Бирхин',source:'official route passport'},
-  'arshan-2':{lat:51.915617,lon:102.429026,label:'Будда Медицины',googleUrl:'https://www.google.com/maps/search/?api=1&query=%D0%91%D1%83%D0%B4%D0%B4%D0%B0%20%D0%9C%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D1%8B%2C%20%D0%90%D1%80%D1%88%D0%B0%D0%BD',yandexUrl:'https://yandex.ru/maps/org/budda_meditsiny/152183331065/',source:'verified map entity + 2026 guide'},
+  'arshan-2':{lat:51.915617,lon:102.429026,label:'Будда Медицины',googleUrl:'https://maps.app.goo.gl/EFbSrY9WXRYZiWza6?g_st=ic',yandexUrl:'https://yandex.ru/maps/org/budda_meditsiny/152183331065/',source:'verified map entities + 2026 guide'},
   'arshan-9':{lat:51.91541,lon:102.42826,label:'Алтарь Сагаан Убгэн',yandexUrl:'https://yandex.ru/maps/org/sagaan_ubgen/7389356205/',source:'verified Yandex map entity'},
   'slud-1':{lat:51.658786,lon:103.724168,label:'Мраморный вокзал Слюдянки',yandexUrl:'https://yandex.ru/maps/org/railway_station/199004009873/',source:'verified Yandex railway-station entity'},
   'slud-10':{lat:51.73206,lon:103.711082,label:'Смотровая на Култукском серпантине',yandexUrl:'https://yandex.ru/maps/org/smotrovaya_ploshchadka/45168199333/',source:'verified Yandex observation-deck entity'},
