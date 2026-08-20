88f06d7da61ccc91a81ca14397386ae40e0703e0
plavnikus <stroy38.ru@gmail.com>
2026-08-20 23:31:19 +0800

    Add Buguldeyka corridor travel cards

--- place-nav.js
+++ place-nav.js
@@ -4,6 +4,7 @@ window.PLACE_NAV={
   'tazh-3':{lat:52.925877,lon:106.759206,label:'Тажеранская обзорная площадка',source:'official route passport'},
   'tazh-4':{lat:52.951578,lon:106.749014,label:'Тажеранские скалы-останцы',source:'official route passport'},
   'tazh-5':{lat:52.719069,lon:106.533435,label:'Залив Бирхин',source:'official route passport'},
+  'tazh-24':{lat:52.790486,lon:104.745721,label:'Усть-Ордынский дацан «Тубдэн Даржелин»',source:'official address + verified cartography'},
   'arshan-2':{lat:51.915617,lon:102.429026,label:'Будда Медицины',googleUrl:'https://maps.app.goo.gl/EFbSrY9WXRYZiWza6?g_st=ic',yandexUrl:'https://yandex.ru/maps/org/budda_meditsiny/152183331065/',source:'verified map entities + 2026 guide'},
   'arshan-9':{lat:51.91541,lon:102.42826,label:'Алтарь Сагаан Убгэн',yandexUrl:'https://yandex.ru/maps/org/sagaan_ubgen/7389356205/',source:'verified Yandex map entity'},
   'slud-1':{lat:51.658786,lon:103.724168,label:'Мраморный вокзал Слюдянки',yandexUrl:'https://yandex.ru/maps/org/railway_station/199004009873/',source:'verified Yandex railway-station entity'},
