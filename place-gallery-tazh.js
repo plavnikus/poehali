bcb3266dde5b7ed743b80f6505d669c094f55553
plavnikus <stroy38.ru@gmail.com>
2026-08-23 00:20:15 +0800

    Add Bayanday museum and church photos

--- place-gallery-tazh.js
+++ place-gallery-tazh.js
@@ -9,6 +9,10 @@ Object.assign(window.GALLERIES||{}, {
     {"src":"https://volkov.irkutsk.ru/content/images/2023/08/AG0A9352panor.jpg","caption":"Панорама устья Бугульдейки · фото Сергей Волков","sourceUrl":"https://volkov.irkutsk.ru/posts/skulptura-bukha-noiona-na-buguldieiskom-mramornom-karierie/","rights":"family-reference"},
     {"src":"https://volkov.irkutsk.ru/content/images/2023/08/IMG_0371.JPG","caption":"Мишкина гора и долина Бугульдейки · фото Сергей Волков","sourceUrl":"https://volkov.irkutsk.ru/posts/skulptura-bukha-noiona-na-buguldieiskom-mramornom-karierie/","rights":"family-reference"}
   ],
+  "tazh-23":[
+    {"src":"https://res.cloudinary.com/dylmxvxrx/image/fetch/c_limit/http%3A//www.ogirk.ru/wp-content/uploads/post/2022/09/tvorec.jpg","caption":"Деревянные скульптуры в Баяндаевском этнографическом музее · фото Андрей Федоров / Областная","sourceUrl":"https://www.ogirk.ru/2022/09/14/mlechnyj-put-novye-nahodki/","rights":"family-reference"},
+    {"src":"https://res.cloudinary.com/dylmxvxrx/image/fetch/c_limit/http%3A//www.ogirk.ru/wp-content/uploads/post/2022/09/mu7a6498_novyj-razmer.jpg","caption":"Русская усадьба Баяндаевского этнографического музея · фото Андрей Федоров / Областная","sourceUrl":"https://www.ogirk.ru/2022/09/14/mlechnyj-put-novye-nahodki/","rights":"family-reference"}
+  ],
   "tazh-25":[
     {"src":"https://upload.wikimedia.org/wikipedia/commons/7/7b/%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B3%D0%BB%D0%B8%D1%84%D1%8B_%D1%83_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BD%D0%B8_%D0%9A%D1%83%D1%80%D1%82%D1%83%D0%BD_12.jpg","thumb":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B3%D0%BB%D0%B8%D1%84%D1%8B_%D1%83_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BD%D0%B8_%D0%9A%D1%83%D1%80%D1%82%D1%83%D0%BD_12.jpg/960px-%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B3%D0%BB%D0%B8%D1%84%D1%8B_%D1%83_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BD%D0%B8_%D0%9A%D1%83%D1%80%D1%82%D1%83%D0%BD_12.jpg","caption":"Петроглифы у деревни Куртун · Александр Деловеров · CC BY-SA 4.0","sourceUrl":"https://commons.wikimedia.org/wiki/File:%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B3%D0%BB%D0%B8%D1%84%D1%8B_%D1%83_%D0%B4%D0%B5%D1%80%D0%B5%D0%B2%D0%BD%D0%B8_%D0%9A%D1%83%D1%80%D1%82%D1%83%D0%BD_12.jpg","rights":"licensed"}
   ],
@@ -19,5 +23,8 @@ Object.assign(window.GALLERIES||{}, {
     {"src":"https://res.cloudinary.com/doyndftsq/image/fetch/c_limit,w_1400/http%3A//www.ogirk.ru/wp-content/uploads/post/2023/05/1-5-scaled.jpg","thumb":"https://res.cloudinary.com/doyndftsq/image/fetch/c_limit,w_900/http%3A//www.ogirk.ru/wp-content/uploads/post/2023/05/1-5-scaled.jpg","caption":"Тургеневка, белорусский праздник Юр’я · фото Матрёна Бизикова / Областная","sourceUrl":"https://www.ogirk.ru/2023/05/31/umytsja-rosoj-i-pogadat/","rights":"family-reference"},
     {"src":"https://res.cloudinary.com/doyndftsq/image/fetch/c_limit,w_1400/http%3A//www.ogirk.ru/wp-content/uploads/post/2023/05/3-3-scaled.jpg","thumb":"https://res.cloudinary.com/doyndftsq/image/fetch/c_limit,w_900/http%3A//www.ogirk.ru/wp-content/uploads/post/2023/05/3-3-scaled.jpg","caption":"Тургеневка и местные белорусские традиции · фото Матрёна Бизикова / Областная","sourceUrl":"https://www.ogirk.ru/2023/05/31/umytsja-rosoj-i-pogadat/","rights":"family-reference"},
     {"src":"https://res.cloudinary.com/doyndftsq/image/fetch/c_limit,w_1400/http%3A//www.ogirk.ru/wp-content/uploads/post/2023/05/6-2-scaled.jpg","thumb":"https://res.cloudinary.com/doyndftsq/image/fetch/c_limit,w_900/http%3A//www.ogirk.ru/wp-content/uploads/post/2023/05/6-2-scaled.jpg","caption":"Живая культурная среда Тургеневки · фото Матрёна Бизикова / Областная","sourceUrl":"https://www.ogirk.ru/2023/05/31/umytsja-rosoj-i-pogadat/","rights":"family-reference"}
+  ],
+  "tazh-34":[
+    {"src":"https://sobory.ru/pic/58200/58239bb.jpg","caption":"Храм Архангела Михаила в Баяндае · Соборы.ру","sourceUrl":"https://sobory.ru/article/?object=58239","rights":"family-reference"}
   ]
 });
