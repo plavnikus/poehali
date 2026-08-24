540e1b04568fafbe8a2b3eedf96c76a414ae9648
plavnikus <stroy38.ru@gmail.com>
2026-08-24 20:27:47 +0800

    Split Buguldeyka into separate direction

---
+++ direction-groups.js
@@ -0,0 +1,3 @@
+const BUGULDEYKA_DIRECTION_IDS=new Set(['tazh-14','tazh-21','tazh-22','tazh-23','tazh-25','tazh-26','tazh-27','tazh-28','tazh-29','tazh-30','tazh-31','tazh-32','tazh-33','tazh-34','tazh-35']);
+P.forEach(p=>{if(BUGULDEYKA_DIRECTION_IDS.has(p.i))p.d='buguldeyka'});
+D.buguldeyka=['Бугульдейка / Баяндай','Баяндай, Косая Степь, Куртун и берег Байкала','🌊','assets/place-photos/tazh-14-1.jpg'];
